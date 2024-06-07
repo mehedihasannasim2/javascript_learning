574a5cd882f7e0f6e8b646fb507357fa1d5e7033
nasim-etlimited <nasim@etlimited.net>
2024-06-07 17:08:44 +0600

    dynamic type of js

---
+++ dynamicTyping.js
@@ -0,0 +1,16 @@
+// js > dynamic
+
+
+let name = 'nasim'; // string
+let age = 30; // Number
+let isApproved = false; // Boolean
+let firstName = undefined;
+let selectedColor = null; 
+
+console.log(typeof name)  // output = string
+
+
+console.log(typeof age) // output = number
+console.log(typeof isApproved) // output = boolean
+console.log(typeof firstName) // output = undefined
+console.log(typeof selectedColor) // output = object

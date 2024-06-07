c5612dd21ba2e014b9216adad9bc7e9b100cc08f
nasim-etlimited <nasim@etlimited.net>
2024-06-07 17:28:24 +0600

    reference type of js & renaming for sorting

---
+++ 1_index.js
@@ -0,0 +1,15 @@
+// javaScripts has Two Types: Primitives/value types & Reference Type.
+// Primitives: String, Number, Boolean, undefined, null
+
+// let name = 'nasim'; // string
+// let age = 30; // Number
+// let isApproved = false; // Boolean
+// let firstName = undefined;
+// let selectedColor = null; 
+
+let person = {
+    name: 'nasim',
+    age: 30
+};
+
+console.log(person)
---
+++ 6_referenceTypes.js
@@ -0,0 +1,18 @@
+// Reference types: Object, Array, Function
+
+let person = {
+    name: 'nasim',
+    age: 30
+};
+
+
+// to change the property
+// Dot notation >>
+person.name= 'mehedi';
+
+// Bracket Notation
+let selection = 'name'
+person[selection] = 'hasan';
+
+console.log(person)
+

7aa462916af394ea0d6856166157f09928d360ee
nasim-etlimited <nasim@etlimited.net>
2024-06-07 12:51:16 +0600

    variables & constraits in js

---
+++ index.js
@@ -0,0 +1,11 @@
+// value will change if it call the variable again
+
+let interestRate = 0.3; 
+interestRate = 1;
+console.log(interestRate)
+
+
+// if we dont need to reassign than we should use CONST
+
+const interestRate2 = 0.3; 
+console.log(interestRate)
---
+++ variables.js
@@ -0,0 +1,17 @@
+// print
+console.log('hello')
+
+
+//var call
+let name = 'nasim'
+console.log(name);
+
+// rules
+// Cannot be a reserved keyword
+// should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hypen (-)
+// Are case-sensitive
+
+let firstName = 'mehedi';
+let lastName =  'nasim';

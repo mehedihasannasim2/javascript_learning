57062f6c2d9839c1cbb3a14bbeab0389f2b6393a
nasim-etlimited <nasim@etlimited.net>
2024-06-08 14:27:55 +0600

    loop in js

---
+++ do_while.js
@@ -0,0 +1,15 @@
+// do while
+let i = 0;
+do {
+    if (i % 2 !== 0) console.log(i);
+    i++
+} while (i <= 5)
+
+
+
+// still output is 9 
+let j = 9;
+do {
+    if (j % 2 !== 0) console.log(j);
+    j++
+} while (j <= 5)
---
+++ for_loop.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 5; i++) {
+    console.log('hello world', i);
+    if (i % 2 !== 0) console.log(i);
+}
+
+// reverse order
+for (let i = 5; i >= 1; i--) {
+    console.log('hello world', i);
+    if (i % 2 !== 0) console.log(i);
+}

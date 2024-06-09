c7d11f500d68c4b0d62addbee8aee82d4fd5ceeb
nasim-etlimited <nasim@etlimited.net>
2024-06-09 14:44:31 +0600

    price range object exercise in js

---
+++ 40_PriceRangeObject.js
@@ -0,0 +1,9 @@
+let priceRanges = [
+    { lebel: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10},
+    { lebel: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20},
+    { lebel: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 30},
+];
+
+let retaurants = [
+    { averagePerPerson: 5}
+]

0efab4ff871f82e8e6475ba20902ce56fb26465c
nasim-etlimited <nasim@etlimited.net>
2024-06-09 12:37:57 +0600

    template literals in js

---
+++ 33_template_literals.js
@@ -0,0 +1,30 @@
+// String primitive
+
+const message = 
+'this is my\n' +
+'\'first\' message';
+
+console.log(message)
+
+// object {}
+// Boolean true, false
+// String '', ""
+// template ``
+
+// Template literal examples
+const another = 
+`this is my
+'first' message`;
+
+console.log(another)
+
+// Template literal examples 2
+const username = 'nasim';
+
+const mailing = 
+`hi ${username} ${2+3},
+thank for reading my mail
+regards, 
+mehedi`;
+
+console.log(mailing)

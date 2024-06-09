c723df5d5f09c9d46a4a8a2f442a2a6b394efd4a
nasim-etlimited <nasim@etlimited.net>
2024-06-09 12:04:04 +0600

    string in js

---
+++ 32_string.js
@@ -0,0 +1,13 @@
+// String primitive
+const message = "this is my first message"
+
+
+// String object
+const another = new String('hi')
+
+console.log(typeof(message))
+console.log(typeof(another))
+
+
+console.log(message.replace('first', 'second'))
+console.log(message);

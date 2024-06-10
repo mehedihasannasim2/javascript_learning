cfa897f52606fa0cff35cfc9e8197964ee80bd4f
nasim-etlimited <nasim@etlimited.net>
2024-06-10 14:16:07 +0600

    include module in js

---
+++ 57_include_module.js
@@ -0,0 +1,14 @@
+const numbers = [1,2,3,4,5];
+
+// console.log(numbers.includes(1));
+function includes(array, searchElement) {
+    for (let n of array)
+        if (n === searchElement)  
+            return true;
+    return false;
+}
+
+console.log(includes(numbers, 3));
+
+
+

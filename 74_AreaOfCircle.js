fddbdb151e0cbd62eb72c648a73feec3d6c2fa3f
nasim-etlimited <nasim@etlimited.net>
2024-06-11 17:01:19 +0600

    area of circle in js

---
+++ 74_AreaOfCircle.js
@@ -0,0 +1,26 @@
+
+// circle.radius = 2;
+// 
+
+const circle = {
+    radius: 5,
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
+
+console.log(circle.area);
+
+ 
+
+
+
+// own try
+
+// function Area(radius) {
+//     return 2 * 3.1416 * radius 
+// }
+
+// console.log(Area(2))
+
+// console.log(circle.area);

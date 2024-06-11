1474dcc215177209a7c71334acdb2ef65f70ca75
nasim-etlimited <nasim@etlimited.net>
2024-06-11 17:19:43 +0600

    Error handling exercise in js

--- 75_ErrorHandling.js
+++ 75_ErrorHandling.js
@@ -1,20 +1,23 @@
-const numbers = [1,2,3,4,5,1]
 
-const count = countOccurrences(numbers, 1);
-console.log(count);
+try {  
+    const numbers = [1,2,3,4,5,1]
+    const count = countOccurrences(null, 1);
+    console.log(count);
+} catch (error) {
+    console.log(error.message);
+}
+
+
 
 function countOccurrences(array, searchElement) {
-    // let counting = 0;
-    // for (let n of array)
-    //     if (n === searchElement)
-    //         counting += 1;
-    // return counting;
+    if (!Array.isArray(array))
+        throw new Error("vul array")
 
-    // using reduce
     return array.reduce((accumulator, current) => {
         const occurrence = (current === searchElement) ? 1 : 0;
         console.log(accumulator, current, searchElement)
         return accumulator + occurrence;
     }, 0);
 }
+
 

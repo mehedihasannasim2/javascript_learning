a66ac4943b451eec3f32cbc9bd8a1a6ba10b278a
nasim-etlimited <nasim@etlimited.net>
2024-06-11 15:15:38 +0600

    global & local variable in js

---
+++ 69_localVsGlobal_scope.js
@@ -0,0 +1,13 @@
+
+// gobal variable
+const color = 'red';
+
+function start () {
+    // local variable
+    const message = 'hi';
+    const color = 'blue';
+    console.log(color);
+
+}
+
+start(); 

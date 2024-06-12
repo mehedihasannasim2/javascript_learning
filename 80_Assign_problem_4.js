00975e659cdc4e21b82fda48ac7a7e2e380bb675
nasim-etlimited <nasim@etlimited.net>
2024-06-12 17:21:48 +0600

    Assignment problem 4 js

--- 80_Assign_problem_4.js
+++ 80_Assign_problem_4.js
@@ -1,3 +1,5 @@
+// Problem 4:
+// Design a TypeScript function reverseArray that takes an array of any type and returns a new array with its elements reversed. Use generics to define types.
 
 
 function reverseArray(array) {

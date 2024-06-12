0f29592f35cefc4935baf7b3f43918d3df4c4ab6
nasim-etlimited <nasim@etlimited.net>
2024-06-12 15:21:58 +0600

    assignment problem 1 in js

--- 77_Assign_problem_1.js
+++ 77_Assign_problem_1.js
@@ -1,1 +1,12 @@
-const repeatString = (str, num) => (i=0, i < num + 1, i++) 
+// Problem 1:
+// Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+
+
+function repeatString(str, times) {
+    let result = ('');
+    for (let i = 0; i < times; i++) {
+       result += str + "\n";
+    }
+    return result;
+ }
+ console.log(repeatString('abc', 3));
